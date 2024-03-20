f0f0205edc106d67b1a2d361962061611e65ce0a
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-20 13:06:54 +0530

    Initial Commit

---
+++ LotsAndHots/LotsAndHotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotsAndHotsApp.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct LotsAndHotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
