5184766acb6609e354114ee6200a650e62f3a96d
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-20 21:30:41 +0530

    created signin view

---
+++ LotsAndHots/UICommon/CountryPickerUI.swift
@@ -0,0 +1,48 @@
+//
+//  CountryPickerUI.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-20.
+//
+
+import SwiftUI
+import CountryPicker
+
+
+struct CountryPickerUI: UIViewControllerRepresentable {
+    
+    @Binding var country: Country?
+       
+    class Coordinator:NSObject, CountryPickerDelegate {
+        var parent: CountryPickerUI
+        
+        init(_ parent: CountryPickerUI) {
+            self.parent = parent
+        }
+        
+        func countryPicker(didSelect country: Country) {
+            parent.country = country
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> some CountryPickerViewController {
+        
+        let countryPicker = CountryPickerViewController()
+        countryPicker.selectedCountry  = "Sri"
+        countryPicker.delegate = context.coordinator
+        
+        return countryPicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    
+}
+
--- LotsAndHots/View/Login/SignInView.swift
+++ LotsAndHots/View/Login/SignInView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import CountryPicker
 
 struct SignInView: View {
     // MARK: - PROPERTY
-    
+    @State var txtMobile: String = ""
+    @State var isShowPicker: Bool = false
+    @State var countryObj: Country?
     
     // MARK: - BODY
     var body: some View {
@@ -26,28 +29,103 @@ struct SignInView: View {
                 Image("sign_in_top")
                     .resizable()
                     .scaledToFill()
-                    .frame(width: .screenWidth, height: .screenWidth - 50)
+                    .frame(width: .screenWidth, height: .screenWidth)
                 
                 Spacer()
             }
             
             ScrollView {
                 
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Step into style: Your ultimate fashion destination awaits!")
                         .font(.customfont(.semibold, fontSize: 26))
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
                         .multilineTextAlignment(.leading)
                         .padding(.bottom, 25)
                     
+                    HStack {
+                        Button {
+                            isShowPicker = true
+                        } label: {
+//                            Image("")
+                            
+                            if let countryObj = countryObj {
+                                
+                                Text( "\( countryObj.isoCode.getFlag())")
+                                    .font(.customfont(.medium, fontSize: 35))
+                                
+                                Text( "+\( countryObj.phoneCode)")
+                                    .font(.customfont(.medium, fontSize: 18))
+                                    .foregroundColor(.primaryText)
+                            }
+                        }
+                        
+                        TextField("Enter Mobile", text: $txtMobile)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                        
+                        
+                    }
+                    
+                    Divider()
+                        .padding(.bottom)
+                    
+                    
+                    Text("Or connect with social media")
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.textTitle)
+                        .multilineTextAlignment(.center)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom, 25)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("google_logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        Text("Continue with Google")
+                            .font(.customfont(.semibold, fontSize: 18))
+                            .foregroundStyle(.white)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+                    .background(Color(hex: "5383EC"))
+                    .cornerRadius(20)
+                    .padding(.bottom, 8)
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("fb_logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        Text("Continue with Facebook")
+                            .font(.customfont(.semibold, fontSize: 18))
+                            .foregroundStyle(.white)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+                    .background(Color(hex: "4A66AC"))
+                    .cornerRadius(20)
                     
                 }
                 .padding(.horizontal, 25)
                 .frame(width: .screenWidth, alignment: .leading)
-                .padding(.top, .topInsets + .screenWidth * 0.55)
+                .padding(.top, .topInsets + .screenWidth )
+                
+                
             }
             
         }
+        .onAppear {
+            self.countryObj = Country(phoneCode: "94", isoCode: "Sri")
+        }
+        .sheet(isPresented: $isShowPicker, content: {
+            CountryPickerUI(country: $countryObj)
+        })
         .navigationTitle("")
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
