75561222191f678655398979e8c4d5a27845b069
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-21 19:00:52 +0530

    main view model updated

--- LotsAndHots/ViewModel/MainViewModel.swift
+++ LotsAndHots/ViewModel/MainViewModel.swift
@@ -24,6 +24,13 @@ class MainViewModel: ObservableObject {
     
     init() {
         
+        if( Utils.UDValueBool(key: Globs.userLogin) ) {
+            // User Login
+            self.setUserData(uDict: Utils.UDValue(key: Globs.userPayload) as? NSDictionary ?? [:] )
+        } else {
+            // User Not Login
+        }
+        
         #if DEBUG
         txtUsername = "user4"
         txtEmail = "test@gmail.com"
