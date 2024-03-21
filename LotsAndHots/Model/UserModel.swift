beb1a61435578e999bbf1907521d5622c6e2b6fe
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-21 17:47:59 +0530

    sign up view created

---
+++ LotsAndHots/Model/UserModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-21.
+//
+
+import SwiftUI
+
+struct UserModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserModel()
+}
---
+++ LotsAndHots/View/Login/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-21.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
