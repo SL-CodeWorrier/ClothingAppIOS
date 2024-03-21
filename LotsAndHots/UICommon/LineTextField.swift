5cdebd46e8451eb9b2e42ba8d8b6f2ff0f9afcfb
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-21 08:14:29 +0530

    linen text field created

--- LotsAndHots/UICommon/LineTextField.swift
+++ LotsAndHots/UICommon/LineTextField.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct LineTextField: View {
+    // MARK: - PROPERTY
+    @Binding var txt: String
+    @State var title: String = "Title"
+    @State var placeholder: String = "Placeholder"
+    
+    
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(title)
+                .font(.customfont(.semibold, fontSize: 16))
+                .foregroundColor(.textTitle)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            TextField(placeholder, text: $txt)
+            
+            Divider()
+        }    }
+}
+
+// MARK: - PREVIEW
+struct LineTextField_Previews: PreviewProvider {
+    @State static var txt: String = ""
+    static var previews: some View {
+        LineTextField(txt: $txt)
+            .padding(20)
     }
 }
 
-#Preview {
-    LineTextField()
-}
+
--- LotsAndHots/View/Login/LoginView.swift
+++ LotsAndHots/View/Login/LoginView.swift
@@ -8,11 +8,96 @@
 import SwiftUI
 
 struct LoginView: View {
+    // MARK: - PROPERTY
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @StateObject var loginVM = MainViewModel.shared;
+    
+    
+    
+    // MARK: - BODY
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .screenWidth, height: .screenWidth)
+            
+            
+            VStack {
+                
+                
+                Image("color_logo")
+                    .frame(width: 40, height: 20)
+                    .padding(.bottom, .screenWidth * 0.012)
+                
+                Spacer()
+                    .frame(height: .screenWidth * 0.15)
+                
+                Text("Loging")
+                    .font(.customfont(.semibold, fontSize: 26))
+                    .foregroundColor(.primaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 5)
+                
+                Text("Enter your emails and passwords")
+                    .font(.customfont(.semibold, fontSize: 16))
+                    .foregroundColor(.secondaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, .screenWidth * 0.1)
+                
+                
+                
+                VStack {
+                    Text("Email")
+                        .font(.customfont(.semibold, fontSize: 16))
+                        .foregroundColor(.textTitle)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    
+                    TextField("Enter your email address", text: $loginVM.txtEmail)
+                    
+                    Divider()
+                }
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets + 64)
+            .padding(.horizontal, 20)
+            .padding(.bottom, .bottomInsets)
+            
+            VStack {
+                HStack {
+                    Button{
+                        mode.wrappedValue.dismiss()
+                    } label: {
+                        Image("back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                           
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets)
+            .padding(.horizontal, 20)
+        }
+        .background(Color.white)
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea()
     }
 }
 
-#Preview {
-    LoginView()
+
+// MARK: - PREVIEW
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
+
