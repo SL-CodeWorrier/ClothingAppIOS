6c4210feffb7c6f86b5b1723f76db15988a83d90
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-22 12:00:10 +0530

    update service call

---
+++ LotsAndHots/Model/TypeModel.swift
@@ -0,0 +1,18 @@
+//
+//  TypeModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-22.
+//
+
+import SwiftUI
+
+struct TypeModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeModel()
+}
