ccb91026df1d2831e167864733750486605c8bef
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-22 09:47:40 +0530

    CREATED section title all

---
+++ LotsAndHots/UICommon/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductSell.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-22.
+//
+
+import SwiftUI
+
+struct ProductSell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductSell()
+}
---
+++ LotsAndHots/UICommon/SectionTitleAll.swift
@@ -0,0 +1,18 @@
+//
+//  SectionTitleAll.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-22.
+//
+
+import SwiftUI
+
+struct SectionTitleAll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionTitleAll()
+}
