a0092ce910067c452ac6ed85331ddc319f6209c1
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-22 11:08:47 +0530

    product model created

---
+++ LotsAndHots/Model/ProductModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-22.
+//
+
+import SwiftUI
+
+struct ProductModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductModel()
+}
---
+++ LotsAndHots/UICommon/CategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCell.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-22.
+//
+
+import SwiftUI
+
+struct CategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCell()
+}
