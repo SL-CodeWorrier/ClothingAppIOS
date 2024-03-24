998fb76ed9a16c306cbe57da00126b3ba859bc34
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-24 17:38:58 +0530

    explore view created

---
+++ LotsAndHots/UICommon/ExploreCategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreCategoryCell.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-24.
+//
+
+import SwiftUI
+
+struct ExploreCategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreCategoryCell()
+}
