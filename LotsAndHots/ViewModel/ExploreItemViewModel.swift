dbcf4cb5c0d1d16bc43ec53e3f159249c92d6c8f
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-24 17:43:58 +0530

    Explore item viewmodel created

---
+++ LotsAndHots/ViewModel/ExploreItemViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreItemViewModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-24.
+//
+
+import SwiftUI
+
+struct ExploreItemViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreItemViewModel()
+}
