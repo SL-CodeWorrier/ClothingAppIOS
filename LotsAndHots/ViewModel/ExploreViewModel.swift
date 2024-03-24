f47f3eadefbb5f58f5e9bef2eb0afb51c67f5e23
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-24 16:59:46 +0530

    update globs

---
+++ LotsAndHots/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreViewModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-24.
+//
+
+import SwiftUI
+
+struct ExploreViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreViewModel()
+}
