cf0968c408787d55ae2f7865ae20010926422347
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-24 20:08:31 +0530

    update cart

---
+++ LotsAndHots/ViewModel/CartViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CartViewModel.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-24.
+//
+
+import SwiftUI
+
+struct CartViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartViewModel()
+}
