b6cb0c886f827e623877dd3602e22560ae01e867
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-24 13:33:51 +0530

    favourite row created

---
+++ LotsAndHots/UICommon/FavouriteRow.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteRow.swift
+//  LotsAndHots
+//
+//  Created by Chathura Aththanayaka on 2024-03-24.
+//
+
+import SwiftUI
+
+struct FavouriteRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteRow()
+}
